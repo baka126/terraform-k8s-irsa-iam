2ab4a3b1b45040ecac013d9be9878c966e5d290b
baka126 <bikashyadav126@gmail.com>
2025-06-04 13:24:19 +0545

    feat/update added output in example

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -8,7 +8,6 @@ module "this" {
   kubernetes_namespace            = var.kubernetes_namespace
   kubernetes_service_account_name = var.kubernetes_service_account_name
   eks_oidc_provider_url           = var.oidc_provider_url
-  aws_account_id                  = data.aws_caller_identity.current.account_id
   role_description                = "IAM Role for Service Account"
 
   existing_policy_attachments_map = {
--- examples/complete/outputs.tf
+++ examples/complete/outputs.tf
@@ -2,6 +2,8 @@
 # Defines the Attribute Reference created infrastructure
 ################################################################################
 
-output "id" {
-  value = null_resource.example.id
+output "role_arn" {
+  description = "The ARN of the created IAM role. Returns null if 'create_role' is false."
+  value       = module.this.iam_role_arn
+
 }
